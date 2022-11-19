a3da1d041b14d9ac5b30c1c59d70f320193b8380
sergeymelekh95 <sergeymelekh95@mail.ru>
2022-11-20 01:26:55 +0400

    try to fix routes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api', usersRouter);
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
