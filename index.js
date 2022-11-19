bd762ee4d5dd88093a69a6560d28ed449cd34a65
sergeymelekh95 <sergeymelekh95@mail.ru>
2022-11-20 00:36:36 +0400

    fix

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
-const path = require('path')
 
 const app = express();
 
@@ -18,8 +17,6 @@ const usersRouter = require('./routers/usersRouter');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-
-app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/api/auth', authRouter);
 app.use('/api', usersRouter);
